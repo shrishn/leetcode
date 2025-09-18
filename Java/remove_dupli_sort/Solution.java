a87b7b4d6d96130de27fc44732196943cc83501a
shrishn <shrishcodes@gmail.com>
2025-09-18 18:30:42 +0000

    Remove Dupli 1

---
+++ Java/remove_dupli_sort/Solution.java
@@ -0,0 +1,31 @@
+public class Solution{
+	public static void main(String []args){
+		int n=removeDuplicates(new int[]{1,1,2});
+		System.out.println(n);
+	}
+	public static int removeDuplicates(int []nums){
+		int i=0;
+		int j=1;
+
+
+		while(i<nums.length-1){
+			//System.out.println(i);
+			if(nums[i]==nums[i+1]){
+				i++;
+									
+			}
+			else
+			{
+				nums[j]=nums[i+1];
+				j++;
+				i++;
+			}
+		}
+		//for (int k=0;k<nums.length;k++){
+		//	System.out.println("h"+nums[k]);
+		//}
+		return j;
+	}
+	
+}
+
